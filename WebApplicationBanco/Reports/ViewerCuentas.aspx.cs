9f0e6c44065f9b6836ca560c23257063a2b432d8
Anthony Junior Gonzalez Hernandez <anthony_gonzalez1@ucne.edu.do>
2019-03-10 18:13:25 -0400

    ARREGLOS REPORT

---
+++ WebApplicationBanco/Reports/ViewerCuentas.aspx.cs
@@ -0,0 +1,29 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplicationBanco.Reports
+{
+    public partial class ViewerCuentas : System.Web.UI.Page
+    {
+        Expression<Func<Cuentas, bool>> filtro = p => true;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                CuentasReportViewer.ProcessingMode = ProcessingMode.Local;
+                CuentasReportViewer.Reset();
+                CuentasReportViewer.LocalReport.ReportPath = Server.MapPath(@"~\Reportes\ListadoCuentas.rdlc");
+                CuentasReportViewer.LocalReport.DataSources.Clear();
+                CuentasReportViewer.LocalReport.DataSources.Add(new ReportDataSource("DepositosDS", BLL.Metodos.FCuentas(filtro)));
+                CuentasReportViewer.LocalReport.Refresh();
+            }
+        }
+    }
+}
